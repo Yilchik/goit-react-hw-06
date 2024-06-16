93cb1e8657ed086bd8a0967c9569dec21649ac78
Yilchik <yulyayulia2@gmail.com>
2024-06-16 21:00:18 +0200

    const

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -17,4 +17,4 @@ const contactsSlice = createSlice({
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const selectContacts = (state) => state.contacts.items;
-export const contactsReducer = contactsSlice.reducer;
+export default contactsSlice.reducer;
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -2,7 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const filtersSlice = createSlice({
   name: "filters",
-  initialState: { name: "" },
+  initialState: {
+    name: "",
+  },
   reducers: {
     changeFilter: (state, action) => {
       state.name = action.payload;
@@ -12,4 +14,4 @@ const filtersSlice = createSlice({
 
 export const { changeFilter } = filtersSlice.actions;
 export const selectNameFilter = (state) => state.filters.name;
-export const filtersReducer = filtersSlice.reducer;
+export default filtersSlice.reducer;
