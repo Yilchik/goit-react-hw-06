63ae30cc34420bd220c9c8a4a5dca6043c5097cb
Yilchik <yulyayulia2@gmail.com>
2024-06-17 20:31:51 +0200

    ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,30 +1,15 @@
-import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsSlice";
+import { useSelector } from "react-redux";
 import css from "./ContactList.module.css";
-import { RiUser3Fill } from "react-icons/ri";
-import { BsTelephoneFill } from "react-icons/bs";
+import { selectContacts } from "../../redux/contactsSlice";
+import Contact from "../Contact/Contact";
 
-const ContactList = ({ contacts }) => {
-  const dispatch = useDispatch();
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
 
   return (
     <ul className={css.list}>
-      {contacts.map(({ id, name, number }) => (
-        <li key={id} className={css.item}>
-          <div className={css.container}>
-            <div className={css.card}>
-              <p>
-                <RiUser3Fill className={css.myIcon} size={20} />
-                {name}
-              </p>
-              <p>
-                <BsTelephoneFill className={css.myIcon} size={20} />
-                {number}
-              </p>
-            </div>
-            <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
-          </div>
-        </li>
+      {contacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
   );
